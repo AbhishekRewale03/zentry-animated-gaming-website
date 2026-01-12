a313fa9ad0337f98f1ebe8838bb436ac6f0d3f9d
AbhishekRewale03 <abhishek.rewale.2004@gmail.com>
2026-01-12 21:55:22 +0530

    Fix case-sensitive component import for Linux build

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
-import Features from './components/FEATURES.JSX'
+import Features from './components/Features'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Story from './components/Story'
+
 
 const App = () => {
   return (
